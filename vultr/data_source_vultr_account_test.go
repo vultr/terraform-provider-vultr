721a84325470999a6a96a24d987dca96f3888150
David Dymko <dymkod@gmail.com>
2019-05-08 08:17:14 -0400

    datasource account

---
+++ vultr/data_source_vultr_account_test.go
@@ -0,0 +1,29 @@
+package vultr
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccVultrAccount(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVultrAccount(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.vultr_account.account", "balance"),
+					resource.TestCheckResourceAttrSet("data.vultr_account.account", "pending_charges"),
+					resource.TestCheckResourceAttrSet("data.vultr_account.account", "last_payment_date"),
+					resource.TestCheckResourceAttrSet("data.vultr_account.account", "last_payment_amount"),
+				),
+			},
+		},
+	})
+}
+
+func testAccVultrAccount() string {
+	return `data "vultr_account" "account" {}`
+}
