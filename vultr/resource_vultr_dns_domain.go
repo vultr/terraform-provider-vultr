e7e859e98a830c4f481329c04a75cc2f301684e0
David Dymko <dymkod@gmail.com>
2019-05-15 12:21:22 -0400

    resource-dns-domain

---
+++ vultr/resource_vultr_dns_domain.go
@@ -0,0 +1,162 @@
+package vultr
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/vultr/govultr"
+)
+
+func resourceVultrDnsDomain() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceVultrDnsDomainCreate,
+		Read:   resourceVultrDnsDomainRead,
+		Update: resourceVultrDnsDomainUpdate,
+		Delete: resourceVultrDnsDomainDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"domain": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.NoZeroValues,
+			},
+			"server_ip": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceVultrDnsDomainCreate(d *schema.ResourceData, meta interface{}) error {
+
+	client := meta.(*Client).govultrClient()
+
+	domain := d.Get("domain").(string)
+	ip := d.Get("server_ip").(string)
+
+	validIp := net.ParseIP(ip)
+	if validIp == nil {
+		return fmt.Errorf("The supplied IP address is invalid : %s", ip)
+	}
+
+	err := client.DNSDomain.Create(context.Background(), domain, ip)
+
+	if err != nil {
+		return fmt.Errorf("Error while creating DNS domain : %s", err)
+	}
+
+	d.SetId(domain)
+
+	return nil
+}
+
+func resourceVultrDnsDomainRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client).govultrClient()
+
+	domains, err := client.DNSDomain.GetList(context.Background())
+
+	if err != nil {
+		return fmt.Errorf("Error getting domains ")
+	}
+
+	domain := &govultr.DNSDomain{}
+	for i := range domains {
+		if domains[i].Domain == d.Id() {
+			domain = &domains[i]
+			break
+		}
+	}
+
+	if domain == nil {
+		log.Printf("[WARN] Removing DNS domain %s because it is gone", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	records, err := client.DNSRecord.GetList(context.Background(), d.Id())
+
+	if err != nil {
+		if strings.Contains(err.Error(), "Invalid domain") {
+			log.Printf("[WARN] Removing DNS domain %s because it is gone", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error getting DNS records for DNS domain %s: %v", d.Id(), err)
+	}
+
+	record := &govultr.DNSRecord{}
+	for i := range records {
+		if records[i].Type == "A" && records[i].Name == "" {
+			record = &records[i]
+			break
+		}
+	}
+
+	if record == nil {
+		log.Printf("[WARN] Removing DNS domain (%s) because it has no default record", d.Id())
+		d.SetId("")
+		return nil
+	}
+
+	d.Set("domain", domain.Domain)
+	d.Set("server_ip", record.Data)
+
+	return nil
+}
+
+func resourceVultrDnsDomainUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client).govultrClient()
+
+	records, err := client.DNSRecord.GetList(context.Background(), d.Id())
+
+	if err != nil {
+		if strings.Contains(err.Error(), "Invalid domain") {
+			log.Printf("[WARN] Removing DNS domain %s because it is gone", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("Error getting DNS records for DNS domain %s: %v", d.Id(), err)
+	}
+
+	record := &govultr.DNSRecord{}
+	for i := range records {
+		if records[i].Type == "A" && records[i].Name == "" {
+			record = &records[i]
+			break
+		}
+	}
+
+	if record == nil {
+		return resourceVultrDnsDomainRead(d, meta)
+	}
+
+	record.Data = d.Get("server_ip").(string)
+	err = client.DNSRecord.Update(context.Background(), d.Id(), record)
+	if err != nil {
+		return fmt.Errorf("Error updating the default DNS record for DNS domain %s : %v", d.Id(), err)
+	}
+
+	return nil
+}
+
+func resourceVultrDnsDomainDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client).govultrClient()
+
+	log.Printf("[INFO] Destroying DNS domain (%s)", d.Id())
+	err := client.DNSDomain.Delete(context.Background(), d.Id())
+
+	if err != nil {
+		return fmt.Errorf("Error destroying DNS domain %s: %v", d.Id(), err)
+	}
+
+	return nil
+}
