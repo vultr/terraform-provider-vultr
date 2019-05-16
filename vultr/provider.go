6d7dbe1415d35dbdb1a6da0a6f4922df4cf17c6d
David Dymko <dymkod@gmail.com>
2019-05-16 08:45:43 -0400

    resource dns record

--- vultr/provider.go
+++ vultr/provider.go
@@ -41,6 +41,7 @@ func Provider() terraform.ResourceProvider {
 
 		ResourcesMap: map[string]*schema.Resource{
 			"vultr_dns_domain":        resourceVultrDnsDomain(),
+			"vultr_dns_record":        resourceVultrDnsRecord(),
 			"vultr_firewall_group":    resourceVultrFirewallGroup(),
 			"vultr_iso_private":       resourceVultrIsoPrivate(),
 			"vultr_snapshot":          resourceVultrSnapshot(),
