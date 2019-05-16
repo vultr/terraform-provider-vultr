4afed269b3088ef43b450fd235ea2673a8bee2be
David Dymko <dymkod@gmail.com>
2019-05-16 11:41:15 -0400

    datasource sshkey test

--- vultr/data_source_vultr_ssh_key_test.go
+++ vultr/data_source_vultr_ssh_key_test.go
@@ -5,46 +5,61 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccVultrSSHKey(t *testing.T) {
+
+	rName := fmt.Sprintf("%s-%d-terraform", acctest.RandString(3), acctest.RandInt())
+	rSSH, _, err := acctest.RandSSHKeyPair("foobar")
+	name := "data.vultr_ssh_key.my_key"
+	if err != nil {
+		t.Fatalf("Error generating test SSH key pair: %s", err)
+	}
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckVultrSSHKey("Terraform Test"),
+				Config: testAccCheckVultrSSHKeyConfig_basic(rName, rSSH),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.vultr_ssh_key.my_key", "name", "Terraform Test"),
-					resource.TestCheckResourceAttrSet("data.vultr_ssh_key.my_key", "ssh_key"),
-					resource.TestCheckResourceAttrSet("data.vultr_ssh_key.my_key", "date_created"),
+					resource.TestCheckResourceAttr(name, "name", rName),
+					resource.TestCheckResourceAttrSet(name, "ssh_key"),
+					resource.TestCheckResourceAttrSet(name, "date_created"),
 				),
 			},
 			{
-				Config:      testAccCheckVultrSSHKey_noResult("foobar"),
-				ExpectError: regexp.MustCompile(`.* data.vultr_ssh_key.my_key: data.vultr_ssh_key.my_key: no results were found`),
+				Config:      testAccCheckVultrSSHKey_noResult(rName),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(".*%s: %s: no results were found", name, name)),
 			},
 		},
 	})
 }
 
-func testAccCheckVultrSSHKey(name string) string {
+func testAccCheckVultrSSHKeyConfig_basic(name, ssh string) string {
 	return fmt.Sprintf(`
 		data "vultr_ssh_key" "my_key" {
-    	filter {
-    	name = "name"
-    	values = ["%s"]
-	}
-  	}`, name)
+    		filter {
+    			name = "name"
+    			values = ["${vultr_ssh_key.foo.name}"]
+			}
+  		}
+
+		resource "vultr_ssh_key" "foo" {
+			name = "%s"
+			ssh_key = "%s"
+		}
+		`, name, ssh)
 }
 
 func testAccCheckVultrSSHKey_noResult(name string) string {
 	return fmt.Sprintf(`
 		data "vultr_ssh_key" "my_key" {
-    	filter {
-    	name = "name"
-    	values = ["%s"]
-	}
-  	}`, name)
+   			filter {
+   				name = "name"
+   				values = ["%s"]
+			}
+ 		}`, name)
 }
