c78e6216fa5bab196a6f1e7bda96b94277be0766
David Dymko <dymkod@gmail.com>
2019-05-16 11:29:42 -0400

    datasource startup test

--- vultr/data_source_vultr_startup_script_test.go
+++ vultr/data_source_vultr_startup_script_test.go
@@ -5,62 +5,60 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccVultrStartupScript(t *testing.T) {
+
+	rName := fmt.Sprintf("%s-terraform-test", acctest.RandString(10))
+	name := "data.vultr_startup_script.my_script"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckVultrStartupScript("Terraform Test"),
+				Config: testAccCheckVultrStartupScriptConfig_base(rName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.vultr_startup_script.my_script", "name", "Terraform Test"),
-					resource.TestCheckResourceAttr("data.vultr_startup_script.my_script", "type", "boot"),
-					resource.TestCheckResourceAttrSet("data.vultr_startup_script.my_script", "script"),
-					resource.TestCheckResourceAttrSet("data.vultr_startup_script.my_script", "date_created"),
-					resource.TestCheckResourceAttrSet("data.vultr_startup_script.my_script", "date_modified"),
+					resource.TestCheckResourceAttr(name, "name", rName),
+					resource.TestCheckResourceAttr(name, "type", "pxe"),
+					resource.TestCheckResourceAttrSet(name, "script"),
+					resource.TestCheckResourceAttrSet(name, "date_created"),
+					resource.TestCheckResourceAttrSet(name, "date_modified"),
 				),
 			},
 			{
-				Config:      testAccCheckVultrStartupScript_noResult("foobar"),
-				ExpectError: regexp.MustCompile(`.* data.vultr_startup_script.my_script: data.vultr_startup_script.my_script: no results were found`),
-			},
-			{
-				Config:      testAccCheckVultrStartupScript_tooManyResults("boot"),
-				ExpectError: regexp.MustCompile(`.* data.vultr_startup_script.my_script: data.vultr_startup_script.my_script: your search returned too many results. Please refine your search to be more specific`),
+				Config:      testAccCheckVultrStartupScript_noResult(rName),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(".*%s: %s: no results were found", name, name)),
 			},
 		},
 	})
 }
 
-func testAccCheckVultrStartupScript(name string) string {
+func testAccCheckVultrStartupScriptConfig_base(name string) string {
 	return fmt.Sprintf(`
 		data "vultr_startup_script" "my_script" {
-    	filter {
-    	name = "name"
-    	values = ["%s"]
-	}
-  	}`, name)
+    		filter {
+    			name = "name"
+    			values = ["${vultr_startup_script.foo.name}"]
+			}
+  		}
+
+		resource "vultr_startup_script" "foo" {
+			name = "%s"
+			type = "pxe"
+			script = "#!/bin/bash\necho hello world > /root/hello"
+		}
+		`, name)
 }
 
 func testAccCheckVultrStartupScript_noResult(name string) string {
 	return fmt.Sprintf(`
 		data "vultr_startup_script" "my_script" {
-    	filter {
-    	name = "name"
-    	values = ["%s"]
-	}
-  	}`, name)
-}
-
-func testAccCheckVultrStartupScript_tooManyResults(disk string) string {
-	return fmt.Sprintf(`
-		data "vultr_startup_script" "my_script" {
-    	filter {
-    	name = "type"
-    	values = ["%s"]
-	}
-  	}`, disk)
+			filter {
+   				name = "name"
+   				values = ["%s"]
+			}
+ 		}`, name)
 }
