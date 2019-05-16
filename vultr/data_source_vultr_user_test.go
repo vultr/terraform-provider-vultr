b47e85aadf15c2cc69b64ead0c02348f81c2bcf0
David Dymko <dymkod@gmail.com>
2019-05-16 11:19:36 -0400

    datasource user test

--- vultr/data_source_vultr_user_test.go
+++ vultr/data_source_vultr_user_test.go
@@ -2,59 +2,88 @@ package vultr
 
 import (
 	"fmt"
+	"github.com/hashicorp/terraform/helper/acctest"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccVultrUser(t *testing.T) {
+func TestAccVultrUser_dataBase(t *testing.T) {
+
+	rEmail := fmt.Sprintf("terraform-%s@vultr.com", acctest.RandString(4))
+	name := "data.vultr_user.admin"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccVultrUser("terraform-datasource@vultr.com"),
+				Config: testAccVultrUserConfig_base(rEmail),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "email", "terraform-datasource@vultr.com"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "name", "Terraform datasourceTest"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.#", "10"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.0", "manage_users"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.1", "subscriptions"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.2", "billing"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.3", "support"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.4", "provisioning"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.5", "dns"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.6", "abuse"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.7", "upgrade"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.8", "firewall"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "acl.9", "alerts"),
-					resource.TestCheckResourceAttr(
-						"data.vultr_user.admin", "api_enabled", "yes"),
-					resource.TestCheckResourceAttrSet("data.vultr_user.admin", "id"),
+					resource.TestCheckResourceAttr(name, "email", rEmail),
+					resource.TestCheckResourceAttr(name, "name", "Terraform AccTests"),
+					resource.TestCheckResourceAttr(name, "acl.#", "10"),
+					resource.TestCheckResourceAttr(name, "acl.0", "manage_users"),
+					resource.TestCheckResourceAttr(name, "acl.1", "subscriptions"),
+					resource.TestCheckResourceAttr(name, "acl.2", "billing"),
+					resource.TestCheckResourceAttr(name, "acl.3", "support"),
+					resource.TestCheckResourceAttr(name, "acl.4", "provisioning"),
+					resource.TestCheckResourceAttr(name, "acl.5", "dns"),
+					resource.TestCheckResourceAttr(name, "acl.6", "abuse"),
+					resource.TestCheckResourceAttr(name, "acl.7", "upgrade"),
+					resource.TestCheckResourceAttr(name, "acl.8", "firewall"),
+					resource.TestCheckResourceAttr(name, "acl.9", "alerts"),
+					resource.TestCheckResourceAttr(name, "api_enabled", "yes"),
+					resource.TestCheckResourceAttrSet(name, "id"),
 				),
+			},
+			{
+				Config:      testAccVultrUserConfig_noResult(rEmail),
+				ExpectError: regexp.MustCompile(fmt.Sprintf(".*%s: %s: no results were found", name, name)),
 			},
 		},
 	})
 }
 
-func testAccVultrUser(email string) string {
-	return fmt.Sprintf(`data "vultr_user" "admin" {
-  			filter {
+func testAccVultrUserConfig_base(email string) string {
+	return fmt.Sprintf(`
+		data "vultr_user" "admin" {
+			filter {
+    			name = "email"
+    			values = ["${vultr_user.admin.email}"]
+  				}
+			}
+
+		resource "vultr_user" "admin" {
+  			name = "Terraform AccTests",
+  			email = "%s"
+  			password = "password",
+  			acl = [
+            	"manage_users",
+            	"subscriptions",
+            	"billing",
+            	"support",
+            	"provisioning",
+            	"dns",
+            	"abuse",
+            	"upgrade",
+            	"firewall",
+            	"alerts"
+  			]
+  			api_enabled = true
+		}
+
+		`, email)
+}
+
+func testAccVultrUserConfig_noResult(email string) string {
+	return fmt.Sprintf(`
+		data "vultr_user" "admin" {
+			filter {
     			name = "email"
     			values = ["%s"]
-  			}
-			}`, email)
+  				}
+			}
+		`, email)
 }
