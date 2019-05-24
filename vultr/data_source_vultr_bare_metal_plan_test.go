92abd1dd12ed010685e79c18b371445bf0687907
David Dymko <dymkod@gmail.com>
2019-05-24 10:16:55 -0400

    firewall group test improvement

--- vultr/data_source_vultr_bare_metal_plan_test.go
+++ vultr/data_source_vultr_bare_metal_plan_test.go
@@ -46,15 +46,6 @@ func testAccCheckVultrBareMetalPlan(name string) string {
   	}`, name)
 }
 
-func testAccCheckVultrBareMetalPlan_noResult(name string) string {
-	return fmt.Sprintf(`
-		data "vultr_bare_metal_plan" "my_bm_plan" {
-    	filter {
-    	name = "name"
-    	values = ["%s"]
-	}
-  	}`, name)
-}
 
 func testAccCheckVultrBareMetalPlan_tooManyResults(ram int) string {
 	return fmt.Sprintf(`
