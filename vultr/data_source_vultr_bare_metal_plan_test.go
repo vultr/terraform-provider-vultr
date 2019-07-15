7b2a659b3976233677c4b2f106f47e4a15f1bfcb
David Dymko <dymkod@gmail.com>
2019-07-15 10:57:57 -0400

    bumped govultr version + udpated test

--- vultr/data_source_vultr_bare_metal_plan_test.go
+++ vultr/data_source_vultr_bare_metal_plan_test.go
@@ -2,7 +2,6 @@ package vultr
 
 import (
 	"fmt"
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
@@ -14,23 +13,19 @@ func TestAccVultrBareMetalPlan(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckVultrBareMetalPlan("32768 MB RAM,4x 240 GB SSD,1.00 TB BW"),
+				Config: testAccCheckVultrBareMetalPlan("32768 MB RAM,2x 240 GB SSD,5.00 TB BW"),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "name", "32768 MB RAM,4x 240 GB SSD,1.00 TB BW"),
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "cpu_count", "12"),
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "cpu_model", "E-2186G"),
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "ram", "32768"),
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "disk", "4x 240 GB SSD"),
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "bandwidth_tb", "1"),
-					resource.TestCheckResourceAttr("data.vultr_bare_metal_plan.my_bm_plan", "plan_type", "SSD"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "name"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "cpu_count"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "cpu_model"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "ram"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "disk"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "bandwidth_tb"),
+					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "plan_type"),
 					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "deprecated"),
 					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "price_per_month"),
 					resource.TestCheckResourceAttrSet("data.vultr_bare_metal_plan.my_bm_plan", "available_locations.#"),
 				),
-			},
-			{
-				Config:      testAccCheckVultrBareMetalPlan_tooManyResults(32768),
-				ExpectError: regexp.MustCompile(`your search returned too many results. Please refine your search to be more specific`),
 			},
 		},
 	})
@@ -44,14 +39,4 @@ func testAccCheckVultrBareMetalPlan(name string) string {
     	values = ["%s"]
 	}
   	}`, name)
-}
-
-func testAccCheckVultrBareMetalPlan_tooManyResults(ram int) string {
-	return fmt.Sprintf(`
-		data "vultr_bare_metal_plan" "my_bm_plan" {
-    	filter {
-    	name = "ram"
-    	values = [%v]
-	}
-  	}`, ram)
 }
