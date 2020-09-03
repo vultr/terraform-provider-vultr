a09f86a3af847b846078d20445c76914b143c534
David Dymko <dymkod@gmail.com>
2020-09-03 15:40:51 -0400

    v2 datasource domain

--- vultr/data_source_vultr_dns_domain.go
+++ vultr/data_source_vultr_dns_domain.go
@@ -1,64 +1,40 @@
 package vultr
 
-//
-//import (
-//	"context"
-//	"errors"
-//	"fmt"
-//
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-//	"github.com/vultr/govultr/v2"
-//)
-//
-//func dataSourceVultrDnsDomain() *schema.Resource {
-//	return &schema.Resource{
-//		Read: dataSourceVultrDnsDomainRead,
-//		Schema: map[string]*schema.Schema{
-//			"domain": {
-//				Type:         schema.TypeString,
-//				Required:     true,
-//				ValidateFunc: validation.NoZeroValues,
-//			},
-//			"date_created": {
-//				Type:     schema.TypeString,
-//				Computed: true,
-//			},
-//		},
-//	}
-//}
-//
-//func dataSourceVultrDnsDomainRead(d *schema.ResourceData, meta interface{}) error {
-//
-//	client := meta.(*Client).govultrClient()
-//
-//	domain := d.Get("domain").(string)
-//
-//	dnsDomains, err := client.DNSDomain.List(context.Background())
-//
-//	if err != nil {
-//		return fmt.Errorf("error getting dns domains: %v", err)
-//	}
-//
-//	dnsList := []govultr.DNSDomain{}
-//
-//	for _, d := range dnsDomains {
-//
-//		if d.Domain == domain {
-//			dnsList = append(dnsList, d)
-//
-//		}
-//	}
-//
-//	if len(dnsList) > 1 {
-//		return errors.New("your search returned too many results. Please refine your search to be more specific")
-//	}
-//
-//	if len(dnsList) < 1 {
-//		return errors.New("no results were found")
-//	}
-//
-//	d.SetId(dnsDomains[0].Domain)
-//	d.Set("date_created", dnsDomains[0].DateCreated)
-//	return nil
-//}
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+)
+
+func dataSourceVultrDnsDomain() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceVultrDnsDomainRead,
+		Schema: map[string]*schema.Schema{
+			"domain": {
+				Type:         schema.TypeString,
+				Required:     true,
+				Description:  "name of the domain",
+				ValidateFunc: validation.NoZeroValues,
+			},
+			"date_created": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceVultrDnsDomainRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client).govultrClient()
+
+	domain, err := client.Domain.Get(context.Background(), d.Get("domain").(string))
+	if err != nil {
+		return fmt.Errorf("error getting dns domains: %v", err)
+	}
+
+	d.SetId(domain.Domain)
+	d.Set("date_created", domain.DateCreated)
+	return nil
+}
--- vultr/data_source_vultr_dns_domain_test.go
+++ vultr/data_source_vultr_dns_domain_test.go
@@ -1,40 +1,40 @@
 package vultr
 
-//
-//import (
-//	"fmt"
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
-//	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-//	"testing"
-//)
-//
-//func TestAccVultrDnsDomain_dataBase(t *testing.T) {
-//	domain := fmt.Sprintf("%s.com", acctest.RandString(6))
-//	name := "data.vultr_dns_domain.my-site"
-//	resource.Test(t, resource.TestCase{
-//		PreCheck:  func() { testAccPreCheck(t) },
-//		Providers: testAccProviders,
-//		Steps: []resource.TestStep{
-//			{
-//				Config: testAccVultrDnsDomainConfig(domain),
-//				Check: resource.ComposeTestCheckFunc(
-//					resource.TestCheckResourceAttrSet(name, "id"),
-//					resource.TestCheckResourceAttr(name, "domain", domain),
-//					resource.TestCheckResourceAttrSet(name, "date_created"),
-//				),
-//			},
-//		},
-//	})
-//}
-//
-//func testAccVultrDnsDomainConfig(domain string) string {
-//	return fmt.Sprintf(`
-//			data "vultr_dns_domain" "my-site" {
-//  				domain = "${vultr_dns_domain.my-site.id}"
-//			}
-//
-//			resource "vultr_dns_domain" "my-site" {
-//  				domain = "%s"
-//  				server_ip = "10.0.0.0"
-//			}`, domain)
-//}
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccVultrDnsDomain_dataBase(t *testing.T) {
+	domain := fmt.Sprintf("%s.com", acctest.RandString(6))
+	name := "data.vultr_dns_domain.my-site"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVultrDnsDomainConfig(domain),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(name, "id"),
+					resource.TestCheckResourceAttr(name, "domain", domain),
+					resource.TestCheckResourceAttrSet(name, "date_created"),
+				),
+			},
+		},
+	})
+}
+
+func testAccVultrDnsDomainConfig(domain string) string {
+	return fmt.Sprintf(`
+			data "vultr_dns_domain" "my-site" {
+ 				domain = "${vultr_dns_domain.my-site.id}"
+			}
+
+			resource "vultr_dns_domain" "my-site" {
+ 				domain = "%s"
+ 				ip = "10.0.0.0"
+			}`, domain)
+}
