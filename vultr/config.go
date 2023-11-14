84842be34b4228640540296bef802af8836cd646
Michael Riley <mriley@vultr.com>
2023-11-14 13:24:35 -0500

    Remove deprecated SDK meta version function usage

--- vultr/config.go
+++ vultr/config.go
@@ -3,13 +3,15 @@ package vultr
 import (
 	"context"
 	"fmt"
+	"runtime/debug"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/meta"
 	"github.com/vultr/govultr/v3"
 	"golang.org/x/oauth2"
 )
+
+const terraformSDKPath string = "github.com/hashicorp/terraform-plugin-sdk/v2"
 
 // Config is the configuration structure used to instantiate Vultr
 type Config struct {
@@ -27,9 +29,26 @@ func (c *Client) govultrClient() *govultr.Client {
 	return c.client
 }
 
+// terraformSDKVersion looks up the module version of the Terraform SDK for use
+// in the User Agent client string
+func terraformSDKVersion() string {
+	i, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "0.0.0"
+	}
+
+	for _, module := range i.Deps {
+		if module.Path == terraformSDKPath {
+			return module.Version
+		}
+	}
+
+	return "0.0.0"
+}
+
 // Client configures govultr and returns an initialized client
 func (c *Config) Client() (*Client, error) {
-	userAgent := fmt.Sprintf("Terraform/%s", meta.SDKVersionString())
+	userAgent := fmt.Sprintf("Terraform/%s", terraformSDKVersion())
 	tokenSrc := oauth2.StaticTokenSource(&oauth2.Token{
 		AccessToken: c.APIKey,
 	})
