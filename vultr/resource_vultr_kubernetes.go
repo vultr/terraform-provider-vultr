312a91f14110813dd2799787d7921a3c7250c2b2
Michael Riley <mriley@vultr.com>
2025-12-17 15:09:48 -0500

    Migrate to dedicated node pool labels and taints

--- vultr/resource_vultr_kubernetes.go
+++ vultr/resource_vultr_kubernetes.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"slices"
 	"strings"
 	"time"
 
@@ -24,112 +25,58 @@ func resourceVultrKubernetes() *schema.Resource {
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
-		Schema: map[string]*schema.Schema{
-			"label": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"region": {
-				Type:             schema.TypeString,
-				ForceNew:         true,
-				Required:         true,
-				DiffSuppressFunc: IgnoreCase,
-			},
-			"version": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"vpc_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-				ForceNew: true,
-			},
-			"ha_controlplanes": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
-				ForceNew: true,
-			},
-			"enable_firewall": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
-				ForceNew: true,
-			},
-
-			"node_pools": {
-				Type:     schema.TypeList,
-				Optional: true,
-				MaxItems: 1,
-				Elem: &schema.Resource{
-					Schema: nodePoolSchema(false),
-				},
-			},
-
-			// Computed fields
-			"date_created": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"cluster_subnet": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"service_subnet": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"ip": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"endpoint": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"status": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"firewall_group_id": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"kube_config": {
-				Description: "Base64 encoded KubeConfig",
-				Type:        schema.TypeString,
-				Computed:    true,
-				Sensitive:   true,
-			},
-			"cluster_ca_certificate": {
-				Type:      schema.TypeString,
-				Computed:  true,
-				Sensitive: true,
-			},
-
-			"client_key": {
-				Type:      schema.TypeString,
-				Computed:  true,
-				Sensitive: true,
-			},
-
-			"client_certificate": {
-				Type:      schema.TypeString,
-				Computed:  true,
-				Sensitive: true,
+		Schema:        resourceVultrKubernetesV1(),
+		SchemaVersion: 1,
+		StateUpgraders: []schema.StateUpgrader{
+			{
+				Type:    resourceVultrKubernetesV0().CoreConfigSchema().ImpliedType(),
+				Upgrade: resourceVultrKubernetesStateUpgradeV0ToV1,
+				Version: 0,
 			},
 		},
 	}
 }
 
+func resourceVultrKubernetesStateUpgradeV0ToV1(ctx context.Context, rawState map[string]interface{}, meta interface{}) (map[string]interface{}, error) { //nolint:lll
+	if len(rawState) == 0 {
+		return rawState, nil
+	}
+
+	migrateState := rawState["node_pools"].([]interface{})[0].(map[string]interface{})
+	migrateState["cluster_id"] = rawState["id"]
+
+	nps, err := resourceVultrKubernetesNodePoolsStateUpgradeV0ToV1(
+		ctx,
+		migrateState,
+		meta,
+	)
+	if err != nil {
+		log.Println("[ERROR] unable to migrate kubernetes node pool state")
+		return rawState, err
+	}
+
+	rawState["node_pools"].([]interface{})[0] = nps
+
+	return rawState, nil
+}
+
 func resourceVultrKubernetesCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*Client).govultrClient()
 
-	var nodePoolReq []govultr.NodePoolReq
-	if np, npOk := d.GetOk("node_pools"); npOk {
-		nodePoolReq = generateNodePool(np)
-	} else {
-		nodePoolReq = nil
+	nodePools := d.Get("node_pools").([]interface{})
+	nodePool := nodePools[0].(map[string]interface{})
+
+	nodePoolReq := []govultr.NodePoolReq{
+		{
+			NodeQuantity: nodePool["node_quantity"].(int),
+			Label:        nodePool["label"].(string),
+			Plan:         nodePool["plan"].(string),
+			Tag:          tfVKEDefault,
+			AutoScaler:   govultr.BoolToBoolPtr(nodePool["auto_scaler"].(bool)),
+			MinNodes:     nodePool["min_nodes"].(int),
+			MaxNodes:     nodePool["max_nodes"].(int),
+			UserData:     nodePool["user_data"].(string),
+		},
 	}
 
 	req := &govultr.ClusterReq{
@@ -155,6 +102,55 @@ func resourceVultrKubernetesCreate(ctx context.Context, d *schema.ResourceData, 
 			"error while waiting for kubernetes cluster %v to be completed: %v", cluster.ID, err)
 	}
 
+	labels := nodePool["labels"].(*schema.Set).List()
+	for i := range labels {
+		labelMap := labels[i].(map[string]interface{})
+		_, _, err := client.Kubernetes.CreateNodePoolLabel(
+			ctx,
+			d.Id(),
+			cluster.NodePools[0].ID,
+			&govultr.NodePoolLabelReq{
+				Key:   labelMap["key"].(string),
+				Value: labelMap["value"].(string),
+			},
+		)
+
+		if err != nil {
+			return diag.Errorf(
+				"error creating label %q for node pool %v on cluster %v : %v",
+				labelMap["key"].(string),
+				cluster.NodePools[0].Label,
+				d.Id(),
+				err,
+			)
+		}
+	}
+
+	taints := nodePool["taints"].(*schema.Set).List()
+	for i := range taints {
+		taintMap := taints[i].(map[string]interface{})
+		_, _, err := client.Kubernetes.CreateNodePoolTaint(
+			ctx,
+			d.Id(),
+			cluster.NodePools[0].ID,
+			&govultr.NodePoolTaintReq{
+				Key:    taintMap["key"].(string),
+				Value:  taintMap["value"].(string),
+				Effect: taintMap["effect"].(string),
+			},
+		)
+
+		if err != nil {
+			return diag.Errorf(
+				"error creating taint %q for node pool %v on cluster %v : %v",
+				taintMap["key"].(string),
+				cluster.NodePools[0].Label,
+				d.Id(),
+				err,
+			)
+		}
+	}
+
 	return resourceVultrKubernetesRead(ctx, d, meta)
 }
 
@@ -178,17 +174,59 @@ func resourceVultrKubernetesRead(ctx context.Context, d *schema.ResourceData, me
 	found := false
 	for i := range vke.NodePools {
 		if tfVKEDefault == vke.NodePools[i].Tag {
-			if err := d.Set("node_pools", flattenNodePool(&vke.NodePools[i])); err != nil {
+			nodePoolData := flattenNodePool(&vke.NodePools[i])
+
+			// add node pool labels
+			labelData, _, err := client.Kubernetes.ListNodePoolLabels(ctx, d.Id(), vke.NodePools[i].ID)
+			if err != nil {
+				return diag.Errorf("error getting cluster (%v) node pool (%v) labels : %v", d.Id(), vke.NodePools[i].ID, err)
+			}
+
+			var labels []map[string]interface{}
+			for j := range labelData {
+				labels = append(labels, map[string]interface{}{
+					"id":    labelData[j].ID,
+					"key":   labelData[j].Key,
+					"value": labelData[j].Value,
+				})
+			}
+
+			nodePoolData[0]["labels"] = labels
+
+			// add node pool taints
+			taintData, _, err := client.Kubernetes.ListNodePoolTaints(ctx, d.Id(), vke.NodePools[i].ID)
+			if err != nil {
+				return diag.Errorf("error getting cluster (%v) node pool (%v) taints : %v", d.Id(), vke.NodePools[i].ID, err)
+			}
+
+			var taints []map[string]interface{}
+			for j := range taintData {
+				taints = append(taints, map[string]interface{}{
+					"id":     taintData[j].ID,
+					"key":    taintData[j].Key,
+					"value":  taintData[j].Value,
+					"effect": taintData[j].Effect,
+				})
+			}
+
+			nodePoolData[0]["taints"] = taints
+
+			if err := d.Set("node_pools", nodePoolData); err != nil {
 				return diag.Errorf("unable to set resource kubernetes `node_pools` read value: %v", err)
 			}
+
 			found = true
 			break
 		}
 	}
+
 	if !found {
-		return diag.Errorf(`unable to set resource kubernetes default node pool with tag %s for %v. 
-	You must set the default tag on one node pool before importing.`,
-			tfVKEDefault, d.Id())
+		return diag.Errorf(`
+unable to set resource kubernetes default node pool with tag %s for %v. 
+You must set the default tag on one node pool before importing.`,
+			tfVKEDefault,
+			d.Id(),
+		)
 	}
 
 	if err := d.Set("region", vke.Region); err != nil {
@@ -266,81 +304,53 @@ func resourceVultrKubernetesUpdate(ctx context.Context, d *schema.ResourceData, 
 	if d.HasChange("node_pools") {
 		oldNP, newNP := d.GetChange("node_pools")
 
-		if len(newNP.([]interface{})) != 0 && len(oldNP.([]interface{})) != 0 {
-			n := newNP.([]interface{})[0].(map[string]interface{})
+		oldNodePool := oldNP.([]interface{})[0]
+		newNodePool := newNP.([]interface{})[0]
 
-			labels := make(map[string]string)
-			for k, v := range n["labels"].(map[string]interface{}) {
-				labels[k] = v.(string)
+		oldNodePoolData := oldNodePool.(map[string]interface{})
+		newNodePoolData := newNodePool.(map[string]interface{})
+
+		req := &govultr.NodePoolReqUpdate{
+			NodeQuantity: newNodePoolData["node_quantity"].(int),
+		}
+
+		if newNodePoolData["auto_scaler"] != oldNodePoolData["auto_scaler"] {
+			req.AutoScaler = govultr.BoolToBoolPtr(newNodePoolData["auto_scaler"].(bool))
+		}
+
+		if newNodePoolData["min_nodes"] != oldNodePoolData["min_nodes"] {
+			req.MinNodes = newNodePoolData["min_nodes"].(int)
+		}
+
+		if newNodePoolData["max_nodes"] != oldNodePoolData["max_nodes"] {
+			req.MaxNodes = newNodePoolData["max_nodes"].(int)
+		}
+
+		if newNodePoolData["user_data"] != oldNodePoolData["user_data"] {
+			req.UserData = govultr.StringToStringPtr(newNodePoolData["user_data"].(string))
+		}
+
+		if _, _, err := client.Kubernetes.UpdateNodePool(ctx, d.Id(), newNodePoolData["id"].(string), req); err != nil {
+			return diag.Errorf("error updating vke node pool %v : %v", d.Id(), err)
+		}
+
+		if d.HasChange("node_pools.0.labels") {
+			oldLabels := oldNodePoolData["labels"].(*schema.Set).List()
+			newLabels := newNodePoolData["labels"].(*schema.Set).List()
+
+			err := updateNodePoolOptions(ctx, client, d.Id(), newNodePoolData["id"].(string), "labels", oldLabels, newLabels)
+			if err != nil {
+				return diag.FromErr(err)
 			}
+		}
 
-			var taints []govultr.Taint
-			taintsList := n["taints"].(*schema.Set).List()
-			for i := range taintsList {
-				taintMap := taintsList[i].(map[string]interface{})
-				taints = append(taints, govultr.Taint{
-					Key:    taintMap["key"].(string),
-					Value:  taintMap["value"].(string),
-					Effect: taintMap["effect"].(string),
-				})
-			}
+		if d.HasChange("node_pools.0.taints") {
+			oldTaints := oldNodePoolData["taints"].(*schema.Set).List()
+			newTaints := newNodePoolData["taints"].(*schema.Set).List()
 
-			req := &govultr.NodePoolReqUpdate{
-				NodeQuantity: n["node_quantity"].(int),
-				AutoScaler:   govultr.BoolToBoolPtr(n["auto_scaler"].(bool)),
-				MinNodes:     n["min_nodes"].(int),
-				MaxNodes:     n["max_nodes"].(int),
-				// Not updating tag for default node pool since it's needed to lookup in terraform
-				Labels:   labels,
-				Taints:   taints,
-				UserData: govultr.StringToStringPtr(n["user_data"].(string)),
-			}
-
-			if _, _, err := client.Kubernetes.UpdateNodePool(ctx, d.Id(), n["id"].(string), req); err != nil {
-				return diag.Errorf("error updating VKE node pool %v : %v", d.Id(), err)
-			}
-		} else if len(newNP.([]interface{})) == 0 && len(oldNP.([]interface{})) != 0 {
-			// if we have an old node pool state but don't have a new node pool state
-			// we can safely assume this is a node pool removal
-
-			n := oldNP.([]interface{})[0].(map[string]interface{})
-
-			if err := client.Kubernetes.DeleteNodePool(ctx, d.Id(), n["id"].(string)); err != nil {
-				return diag.Errorf("error deleting VKE node pool %v : %v", d.Id(), err)
-			}
-		} else if len(newNP.([]interface{})) != 0 && len(oldNP.([]interface{})) == 0 {
-			// if we don't have an old node pool state but have a new node pool state
-			// we can safely assume this is a new node pool creation
-			n := newNP.([]interface{})[0].(map[string]interface{})
-
-			labels := make(map[string]string)
-			for k, v := range n["labels"].(map[string]interface{}) {
-				labels[k] = v.(string)
-			}
-
-			var taints []govultr.Taint
-			taintsList := n["taints"].(*schema.Set).List()
-			for i := range taintsList {
-				taintMap := taintsList[i].(map[string]interface{})
-				taints = append(taints, govultr.Taint{
-					Key:    taintMap["key"].(string),
-					Value:  taintMap["value"].(string),
-					Effect: taintMap["effect"].(string),
-				})
-			}
-
-			req := &govultr.NodePoolReq{
-				NodeQuantity: n["node_quantity"].(int),
-				Tag:          tfVKEDefault,
-				Plan:         n["plan"].(string),
-				Label:        n["label"].(string),
-				Labels:       labels,
-				Taints:       taints,
-				UserData:     n["user_data"].(string),
-			}
-
-			if _, _, err := client.Kubernetes.CreateNodePool(ctx, d.Id(), req); err != nil {
-				return diag.Errorf("error creating VKE node pool %v : %v", d.Id(), err)
+			err := updateNodePoolOptions(ctx, client, d.Id(), newNodePoolData["id"].(string), "taints", oldTaints, newTaints)
+			if err != nil {
+				return diag.FromErr(err)
 			}
 		}
 	}
@@ -368,47 +378,6 @@ func resourceVultrKubernetesDelete(ctx context.Context, d *schema.ResourceData, 
 		return diag.Errorf("error deleting VKE %v : %v", d.Id(), err)
 	}
 	return nil
-}
-
-func generateNodePool(pools interface{}) []govultr.NodePoolReq {
-	var npr []govultr.NodePoolReq
-	pool := pools.([]interface{})
-	for _, p := range pool {
-		r := p.(map[string]interface{})
-
-		labels := make(map[string]string)
-		for k, v := range r["labels"].(map[string]interface{}) {
-			labels[k] = v.(string)
-		}
-
-		var taints []govultr.Taint
-		taintsList := r["taints"].(*schema.Set).List()
-		for i := range taintsList {
-			taintMap := taintsList[i].(map[string]interface{})
-			taints = append(taints, govultr.Taint{
-				Key:    taintMap["key"].(string),
-				Value:  taintMap["value"].(string),
-				Effect: taintMap["effect"].(string),
-			})
-		}
-
-		t := govultr.NodePoolReq{
-			NodeQuantity: r["node_quantity"].(int),
-			Label:        r["label"].(string),
-			Plan:         r["plan"].(string),
-			Tag:          tfVKEDefault,
-			AutoScaler:   govultr.BoolToBoolPtr(r["auto_scaler"].(bool)),
-			MinNodes:     r["min_nodes"].(int),
-			MaxNodes:     r["max_nodes"].(int),
-			Labels:       labels,
-			Taints:       taints,
-			UserData:     r["user_data"].(string),
-		}
-
-		npr = append(npr, t)
-	}
-
-	return npr
 }
 
 func waitForVKEAvailable(ctx context.Context, d *schema.ResourceData, target string, pending []string, attribute string, meta interface{}) (interface{}, error) { //nolint:lll
@@ -462,20 +431,6 @@ func flattenNodePool(np *govultr.NodePool) []map[string]interface{} {
 		instances = append(instances, n)
 	}
 
-	labels := make(map[string]interface{})
-	for k, v := range np.Labels {
-		labels[k] = v
-	}
-
-	var taints []map[string]interface{}
-	for i := range np.Taints {
-		taints = append(taints, map[string]interface{}{
-			"key":    np.Taints[i].Key,
-			"value":  np.Taints[i].Value,
-			"effect": np.Taints[i].Effect,
-		})
-	}
-
 	pool := map[string]interface{}{
 		"label":         np.Label,
 		"plan":          np.Plan,
@@ -489,12 +444,156 @@ func flattenNodePool(np *govultr.NodePool) []map[string]interface{} {
 		"auto_scaler":   np.AutoScaler,
 		"min_nodes":     np.MinNodes,
 		"max_nodes":     np.MaxNodes,
-		"labels":        labels,
-		"taints":        taints,
 		"user_data":     np.UserData,
 	}
 
 	nodePools = append(nodePools, pool)
 
 	return nodePools
+}
+
+func updateNodePoolOptions(ctx context.Context, client *govultr.Client, clusterID, nodePoolID, optionKind string, oldData, newData []interface{}) error { //nolint:lll
+	type optionData struct {
+		Create   bool
+		Delete   bool
+		OptionID string
+		Key      string
+		Value    string
+		Effect   string
+	}
+
+	oldOptionData := []optionData{}
+	optionRequests := []optionData{}
+	for i := range oldData {
+		oldOption := oldData[i].(map[string]interface{})
+
+		oldData := optionData{
+			OptionID: oldOption["id"].(string),
+			Key:      oldOption["key"].(string),
+		}
+
+		oldOptionData = append(oldOptionData, oldData)
+	}
+
+	for i := range newData {
+		newOption := newData[i].(map[string]interface{})
+
+		newRequest := optionData{
+			Key:    newOption["key"].(string),
+			Value:  newOption["value"].(string),
+			Create: true,
+			Delete: false,
+		}
+
+		if optionKind == "taints" {
+			newRequest.Effect = newOption["effect"].(string)
+		}
+
+		oldIndex := slices.IndexFunc(oldOptionData, func(o optionData) bool { return o.Key == newRequest.Key })
+
+		if oldIndex >= 0 {
+			// delete the old option in the process
+			newRequest.Delete = true
+			newRequest.OptionID = oldOptionData[oldIndex].OptionID
+		}
+
+		optionRequests = append(optionRequests, newRequest)
+	}
+
+	// mark delete options not in changed data
+	for i := range oldOptionData {
+		if !slices.ContainsFunc(optionRequests, func(o optionData) bool { return o.Key == oldOptionData[i].Key }) {
+			optionRequests = append(optionRequests, optionData{
+				OptionID: oldOptionData[i].OptionID,
+				Delete:   true,
+				Create:   false,
+			})
+		}
+	}
+
+	for i := range optionRequests {
+		switch optionKind {
+		case "labels":
+			if optionRequests[i].Delete {
+				err := client.Kubernetes.DeleteNodePoolLabel(
+					ctx,
+					clusterID,
+					nodePoolID,
+					optionRequests[i].OptionID,
+				)
+				if err != nil {
+					return fmt.Errorf(
+						"error deleting label %q from vke %q node pool %q during option update : %w",
+						optionRequests[i].Key,
+						clusterID,
+						nodePoolID,
+						err,
+					)
+				}
+			}
+
+			if optionRequests[i].Create {
+				_, _, err := client.Kubernetes.CreateNodePoolLabel(
+					ctx,
+					clusterID,
+					nodePoolID,
+					&govultr.NodePoolLabelReq{
+						Key:   optionRequests[i].Key,
+						Value: optionRequests[i].Value,
+					},
+				)
+				if err != nil {
+					return fmt.Errorf(
+						"error creating label %q on vke %q node pool %q during option update : %w",
+						optionRequests[i].Key,
+						clusterID,
+						nodePoolID,
+						err,
+					)
+				}
+			}
+		case "taints":
+			if optionRequests[i].Delete {
+				err := client.Kubernetes.DeleteNodePoolTaint(
+					ctx,
+					clusterID,
+					nodePoolID,
+					optionRequests[i].OptionID,
+				)
+				if err != nil {
+					return fmt.Errorf(
+						"error deleting taint %q from vke %q node pool %q during option update : %w",
+						optionRequests[i].Key,
+						clusterID,
+						nodePoolID,
+						err,
+					)
+				}
+			}
+
+			if optionRequests[i].Create {
+				_, _, err := client.Kubernetes.CreateNodePoolTaint(
+					ctx,
+					clusterID,
+					nodePoolID,
+					&govultr.NodePoolTaintReq{
+						Key:    optionRequests[i].Key,
+						Value:  optionRequests[i].Value,
+						Effect: optionRequests[i].Effect,
+					},
+				)
+				if err != nil {
+					return fmt.Errorf(
+						"error creating label %q on vke %q node pool %q during update : %w",
+						optionRequests[i].Key,
+						clusterID,
+						nodePoolID,
+						err,
+					)
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- vultr/vke.go
+++ vultr/vke.go
@@ -2,6 +2,7 @@ package vultr
 
 import (
 	"encoding/base64"
+	"maps"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
@@ -27,43 +28,44 @@ type KubeConfig struct {
 	} `yaml:"users"`
 }
 
-func nodePoolSchema(isNodePool bool) map[string]*schema.Schema {
-	s := map[string]*schema.Schema{
-		"label": {
-			Type:     schema.TypeString,
-			Required: true,
-		},
-		"plan": {
-			Type:     schema.TypeString,
-			Required: true,
-		},
-		"node_quantity": {
-			Type:         schema.TypeInt,
-			ValidateFunc: validation.IntAtLeast(1),
-			Required:     true,
-		},
-		"auto_scaler": {
-			Type:     schema.TypeBool,
-			Optional: true,
-			Default:  false,
-		},
-		"min_nodes": {
-			Type:     schema.TypeInt,
-			Optional: true,
-			Default:  1,
-		},
-		"max_nodes": {
-			Type:     schema.TypeInt,
-			Optional: true,
-			Default:  1,
-		},
+func resourceVultrKubernetesV1() map[string]*schema.Schema {
+	schemaV0 := resourceVultrKubernetesV0().Schema
+	schemaV1 := map[string]*schema.Schema{}
+	maps.Copy(schemaV1, schemaV0)
+
+	schemaV1["node_pools"].Elem.(*schema.Resource).Schema = resourceVultrKubernetesNodePoolsV1(false)
+
+	return schemaV1
+}
+
+func resourceVultrKubernetesNodePoolsV1(isNodePool bool) map[string]*schema.Schema {
+	schemaV0 := resourceVultrKubernetesNodePoolsV0(isNodePool).Schema
+	schemaV1 := map[string]*schema.Schema{}
+
+	schemaLabels := map[string]*schema.Schema{
 		"labels": {
-			Type:     schema.TypeMap,
+			Type:     schema.TypeSet,
 			Optional: true,
-			Elem: &schema.Schema{
-				Type: schema.TypeString,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"key": {
+						Type:     schema.TypeString,
+						Required: true,
+					},
+					"value": {
+						Type:     schema.TypeString,
+						Required: true,
+					},
+					"id": {
+						Type:     schema.TypeString,
+						Computed: true,
+					},
+				},
 			},
 		},
+	}
+
+	schemaTaints := map[string]*schema.Schema{
 		"taints": {
 			Type:     schema.TypeSet,
 			Optional: true,
@@ -81,48 +83,7 @@ func nodePoolSchema(isNodePool bool) map[string]*schema.Schema {
 						Type:     schema.TypeString,
 						Required: true,
 					},
-				},
-			},
-		},
-		"user_data": {
-			Type:     schema.TypeString,
-			Optional: true,
-		},
-		//computed fields
-		"id": {
-			Type:     schema.TypeString,
-			Computed: true,
-		},
-		"date_created": {
-			Type:     schema.TypeString,
-			Computed: true,
-		},
-		"date_updated": {
-			Type:     schema.TypeString,
-			Computed: true,
-		},
-		"status": {
-			Type:     schema.TypeString,
-			Computed: true,
-		},
-		"nodes": {
-			Type:     schema.TypeList,
-			Computed: true,
-			Elem: &schema.Resource{
-				Schema: map[string]*schema.Schema{
 					"id": {
-						Type:     schema.TypeString,
-						Computed: true,
-					},
-					"date_created": {
-						Type:     schema.TypeString,
-						Computed: true,
-					},
-					"label": {
-						Type:     schema.TypeString,
-						Computed: true,
-					},
-					"status": {
 						Type:     schema.TypeString,
 						Computed: true,
 					},
@@ -131,21 +92,230 @@ func nodePoolSchema(isNodePool bool) map[string]*schema.Schema {
 		},
 	}
 
+	maps.Copy(schemaV0, schemaLabels)
+	maps.Copy(schemaV0, schemaTaints)
+	maps.Copy(schemaV1, schemaV0)
+
+	return schemaV1
+}
+
+func resourceVultrKubernetesV0() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"label": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"region": {
+				Type:             schema.TypeString,
+				ForceNew:         true,
+				Required:         true,
+				DiffSuppressFunc: IgnoreCase,
+			},
+			"version": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"ha_controlplanes": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+				ForceNew: true,
+			},
+			"enable_firewall": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+				ForceNew: true,
+			},
+			"node_pools": {
+				Type:     schema.TypeList,
+				Required: true,
+				MaxItems: 1,
+				Elem:     resourceVultrKubernetesNodePoolsV0(false),
+			},
+			// cluster computed fields
+			"date_created": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cluster_subnet": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"service_subnet": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ip": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"endpoint": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"firewall_group_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"kube_config": {
+				Description: "Base64 encoded KubeConfig",
+				Type:        schema.TypeString,
+				Computed:    true,
+				Sensitive:   true,
+			},
+			"cluster_ca_certificate": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+
+			"client_key": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+
+			"client_certificate": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: true,
+			},
+		},
+	}
+}
+
+func resourceVultrKubernetesNodePoolsV0(isNodePool bool) *schema.Resource {
+	s := &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"label": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"plan": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"node_quantity": {
+				Type:         schema.TypeInt,
+				ValidateFunc: validation.IntAtLeast(1),
+				Required:     true,
+			},
+			"auto_scaler": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"min_nodes": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  1,
+			},
+			"max_nodes": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				Default:  1,
+			},
+			"labels": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"taints": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"key": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"value": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"effect": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+			"user_data": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			// computed fields
+			"id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"date_created": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"date_updated": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"nodes": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"date_created": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"label": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
 	if isNodePool {
-		s["cluster_id"] = &schema.Schema{
+		s.Schema["cluster_id"] = &schema.Schema{
 			Type:         schema.TypeString,
 			Required:     true,
 			ValidateFunc: validation.NoZeroValues,
 			ForceNew:     true,
 		}
-		s["tag"] = &schema.Schema{
+		s.Schema["tag"] = &schema.Schema{
 			Type:     schema.TypeString,
 			Optional: true,
 		}
 	} else {
 		// Make tags unmodifiable for the vultr_kubernetes resource
 		// This lets us know which node pool was part of the vultr_kubernetes resource
-		s["tag"] = &schema.Schema{
+		s.Schema["tag"] = &schema.Schema{
 			Type:     schema.TypeString,
 			Computed: true,
 		}
