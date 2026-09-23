d25be0207cdf57199954c7553946af0b035593ac
Michael Riley <mriley@vultr.com>
2026-09-23 11:14:47 -0400

    Add optional debug flags to main

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/vultr/terraform-provider-vultr/vultr"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: vultr.Provider,
-	})
+	var debug bool
+	var addr string
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.StringVar(&addr, "addr", "", "provider address to match when testing or debugging the provider")
+	flag.Parse()
+
+	if debug {
+		if addr == "" {
+			panic("cannot run in debug without a provider addr: use the -addr flag")
+		}
+
+		plugin.Serve(&plugin.ServeOpts{
+			Debug:        debug,
+			ProviderAddr: addr,
+			ProviderFunc: vultr.Provider,
+		})
+
+	} else {
+		plugin.Serve(&plugin.ServeOpts{
+			ProviderFunc: vultr.Provider,
+		})
+	}
 }
